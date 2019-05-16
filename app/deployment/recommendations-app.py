86e5cbb0bba1e79cff77fc554ee7329abfd990ca
DelphianCalamity <kelkost@yahoo.gr>
2019-05-16 21:12:33 +0300

    changes to app

---
+++ app/deployment/recommendations-app.py
@@ -0,0 +1,46 @@
+"""Main module for App Engine app."""
+
+from flask import Flask, jsonify, request, render_template
+from recommendations import Recommendations
+
+app = Flask(__name__)
+rec_util = Recommendations()
+DEFAULT_RECS = 5
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/recommendation', methods=['GET'])
+def recommendation():
+    """Given a user id, return a list of recommended movie ids."""
+    user_id = request.args.get('userId')
+    num_recs = request.args.get('numRecs')
+
+    # validate args
+    if user_id is None:
+        return 'No User Id provided.', 400
+    if num_recs is None:
+        num_recs = DEFAULT_RECS
+    try:
+        uid_int = int(user_id)
+        nrecs_int = int(num_recs)
+    except:
+        return 'User id and number of recs arguments must be integers.', 400
+
+    # get recommended movies
+#     rec_list = rec_util.get_recommendations(uid_int, nrecs_int)
+
+    if rec_list is None:
+        return 'User Id not found : %s' % user_id, 400
+
+    json_response = jsonify({'movies': [str(i) for i in rec_list]})
+    return json_response, 200
+
+
+@app.route('/readiness_check', methods=['GET'])
+def readiness_check():
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
